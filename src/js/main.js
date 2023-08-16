c0dac0ea6ab0a7f057ac869e6d045d474165ff8a
Serik <askaroff_serik@mail.ru>
2023-08-16 11:11:40 +0300

    add: tabs

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,11 @@
-console.log('Hello World!');
+import tabs from './modules/tabs';
+
+window.addEventListener('DOMContentLoaded', () => {
+  tabs('.glazing_slider', '.glazing_block', '.glazing_content', 'active');
+  tabs(
+    '.decoration_slider',
+    '.no_click',
+    '.decoration_content > div > div',
+    'after_click'
+  );
+});
