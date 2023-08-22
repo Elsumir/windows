f1cf1037473d00ffcd0d42f85f9b7fc7c41ef3dd
Serik <askaroff_serik@mail.ru>
2023-08-22 16:42:28 +0300

    add: fix_bug

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -9,6 +9,7 @@ export const modals = () => {
     const modal = document.querySelector(modalSelector);
     const close = document.querySelector(closeSelector);
     const windows = document.querySelectorAll('[data-modal]');
+    const scroll = calcScroll();
 
     const windowsClose = () => {
       windows.forEach((window) => (window.style.display = 'none'));
@@ -24,12 +25,14 @@ export const modals = () => {
 
         modal.style.display = 'block';
         document.body.style.overflow = 'hidden';
+        document.body.style.marginRight = `${scroll}px`;
       });
     });
 
     const closeModal = () => {
       modal.style.display = 'none';
       document.body.style.overflow = '';
+      document.body.style.marginRight = `0px`;
     };
 
     document.addEventListener('keydown', (e) => {
@@ -56,6 +59,21 @@ export const modals = () => {
       document.querySelector(selector).style.display = 'block';
       document.body.style.overflow = '';
     }, time);
+  };
+
+  const calcScroll = () => {
+    const div = document.createElement('div');
+
+    div.style.width = '50px';
+    div.style.height = '50px';
+    div.style.overflowY = 'scroll';
+    div.style.visibility = 'hidden';
+
+    document.body.appendChild(div);
+    const scrollWidth = div.offsetWidth - div.clientWidth;
+    div.remove();
+
+    return scrollWidth;
   };
 
   bindModal({
